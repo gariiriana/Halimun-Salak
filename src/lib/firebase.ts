007ca7b5d570aede01c13b59bdacdeb7f91d3c9f
Gari Iriana <gariiriana14@gmail.com>
2026-06-05 10:14:38 +0700

    security: refactor firebase config to use environment variables, update gitignore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBno1O1G9ZzVX08z8LOXZT47MhDwk-hq5k",
-  authDomain: "halimun-salak.firebaseapp.com",
-  projectId: "halimun-salak",
-  storageBucket: "halimun-salak.firebasestorage.app",
-  messagingSenderId: "689351235702",
-  appId: "1:689351235702:web:eaf75336f78692d8f5e757",
-  measurementId: "G-160JYRJMHE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
